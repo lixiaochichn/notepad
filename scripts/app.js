a34187ab53a67ffaa6317c2e7046f61c38f31a23
lixiaochichn@gmail.com <lixiaochichn@gmail.com>
2017-08-25 00:28:22 +0800

    add001

--- scripts/app.js
+++ scripts/app.js
@@ -10,6 +10,7 @@ document.addEventListener('DOMContentLoaded', function () {
     let $header = document.querySelector('.header');
     let $padbody = document.querySelector('.padbody');
     let $padcover = document.querySelectorAll('.padcover');
+    let $centertitle = document.querySelector('.centertitle');
     let $parent;
     let num = 4;
 
@@ -26,34 +27,41 @@ document.addEventListener('DOMContentLoaded', function () {
 
         // console.log($parent);
         if (target.matches('.padlist')) {
-
-
-
-            // 点击的便签翻转            
-            $parent.classList.remove('home');
-            $parent.classList.add('flip');
-            // $parent.classList.add('show');
-
-            // 标题栏变更为箭头和垃圾桶
-            $leftbutton.classList.remove('fa-cog');
-            $leftbutton.classList.add('fa-arrow-left');
-
-            $rightbutton.classList.remove('fa-pencil-square-o');
-            $rightbutton.classList.add('fa-trash');
-
-            removeorder();
-
-            // 点击的便签排到首位
-            $parent.parentElement.classList.add('zindex');
-            setTimeout(function () {
-                $parent.parentElement.classList.add('order');
-
-            }, 150);
-
+            editmode($parent);
         };
-
     }, true);
 
+    function editmode(targetlist) {
+        $centertitle.innerHTML = `<div class="centertitle">Edit</div>`;
+        // 点击的便签翻转            
+        targetlist.classList.remove('home');
+        targetlist.classList.add('flip');
+
+        // 标题栏变更为箭头和垃圾桶
+        $leftbutton.classList.remove('fa-cog');
+        $leftbutton.classList.add('fa-arrow-left');
+
+        $rightbutton.classList.remove('fa-pencil-square-o');
+        $rightbutton.classList.add('fa-trash');
+        removeorder();
+
+        // 点击的便签排到首位
+        targetlist.parentElement.classList.add('zindex');
+        setTimeout(function () {
+            targetlist.parentElement.classList.add('order');
+
+        }, 150);
+    };
+
+    function listmode() {
+        $centertitle.innerHTML = `<div class="centertitle">All</div>`;
+        
+        $leftbutton.classList.remove('fa-arrow-left');
+        $leftbutton.classList.add('fa-cog');
+
+        $rightbutton.classList.remove('fa-trash');
+        $rightbutton.classList.add('fa-pencil-square-o');
+    };
 
     function removeorder() {
         $padcover.forEach(function (list) {
@@ -89,11 +97,7 @@ document.addEventListener('DOMContentLoaded', function () {
             $parent.classList.remove('flip');
             $parent.classList.add('home');
 
-            $leftbutton.classList.remove('fa-arrow-left');
-            $leftbutton.classList.add('fa-cog');
-
-            $rightbutton.classList.remove('fa-trash');
-            $rightbutton.classList.add('fa-pencil-square-o');
+            listmode();
         };
 
         //点击铅笔新增便签
@@ -108,14 +112,30 @@ document.addEventListener('DOMContentLoaded', function () {
                 <textarea class="padback">${num}正因为人生没有意义，才值得一过。人生、宇宙这些东西不能深想。你想想，一个人来了几十年之后，消失了，就像没有存在一样，其实挺可怕的。但一个有灵魂的人，是不可能不想这些事情的。我现在做的就是如何应对‘生命无意义’这件事。</textarea>
             </div>
         </div>`;
-            $padbody.innerHTML += html.substring(0);
+            
+            $padbody.innerHTML = html.substring(0) + $padbody.innerHTML;
             num++;
             $padcover = document.querySelectorAll('.padcover');
-            console.log($padcover[$padcover.length - 1]);
-            $padcover[$padcover.length - 1].click();
+            $parent = $padcover[0].children[0];
+            // console.log();
+            // $padcover[0].children[0].click();
 
-
+            setTimeout(function () {
+                editmode($parent);
+            }, 0);
         };
+
+        //点击垃圾桶删除
+        if (target.matches('.fa-trash')) {
+            // $parent.classList.remove('flip');
+            // $parent.classList.add('home');
+            $parent.parentElement.classList.add('translate3d');
+            setTimeout(function () {
+                $parent.parentElement.parentElement.removeChild($parent.parentElement);
+            }, 490);
+            listmode();
+        };
+
 
     });
 
